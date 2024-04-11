4506a4f2fcc71c91747c300fd8d78ff92829cf15
dnavmoy <dnavmoy119@g.educaand.es>
2024-04-11 12:45:10 +0200

    probando cosas

--- src/main/java/daw/Ventana11.java
+++ src/main/java/daw/Ventana11.java
@@ -15,14 +15,15 @@ public class Ventana11 {
     public static void main(String[] args) {
         
         JFrame frame = new JFrame("App");
+        frame.setSize(800,600);
 	// La ventana no se puede redimensionar
-	frame.setResizable(true);
+	frame.setResizable(false);
 	// Posición de la ventana
     	frame.setLocationRelativeTo(null);
 	// Incluimos el panel en la ventana
     	frame.add(new Saludo());
        // Ajusta el frame al contenido
-       frame.pack();
+        frame.pack();
 	// Hacemos visible la ventana
     	frame.setVisible(true);
 	//Acción por defecto al pulsar el botón de cierre de la ventana
